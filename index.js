b6fd14480c54db65d265371ba2bba63110709144
mustafas <mustafas@imtac.com>
2021-11-02 12:29:53 +0530

    merge conflicts

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const add = (a, b) => {
 
 add(10, 21)
 
-<<<<<<< HEAD
 const sub = (a, b) => a - b
-=======
-const sub = (a, b) => {
-  console.log({ a, b })
-  return a - b
-}
->>>>>>> dev
 
 sub(10, 20)
